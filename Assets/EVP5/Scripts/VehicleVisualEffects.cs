111007f3e1fac682617e3a988637f6a82c6b91a7
Associate Tech Lead (Frontend) <iamdipankarj@gmail.com>
2023-09-14 17:39:33 +0530

    Fix Lerp issue of steering wheel

--- Assets/EVP5/Scripts/VehicleVisualEffects.cs
+++ Assets/EVP5/Scripts/VehicleVisualEffects.cs
@@ -1,64 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace EVP
-{
+namespace EVP {
 
-[RequireComponent(typeof(VehicleController))]
-public class VehicleVisualEffects : MonoBehaviour
-	{
-	[Header("Steering wheel")]
-	public Transform steeringWheel;
-	public float degreesOfRotation = 420.0f;
+  [RequireComponent(typeof(VehicleController))]
+  public class VehicleVisualEffects : MonoBehaviour {
+    [Header("Steering wheel")]
+    public Transform steeringWheel;
+    public float degreesOfRotation = 420.0f;
 
-	[Header("Brake lights")]
-	public Renderer brakesRenderer;
-	public int brakesMaterialIndex;
-	public Material brakesOnMaterial;
-	public Material brakesOffMaterial;
+    [Header("Brake lights")]
+    public Renderer brakesRenderer;
+    public int brakesMaterialIndex;
+    public Material brakesOnMaterial;
+    public Material brakesOffMaterial;
 
 
-	VehicleController m_vehicle;
-	bool m_prevBrakes = false;
+    VehicleController m_vehicle;
+    bool m_prevBrakes = false;
 
 
-	void OnEnable ()
-		{
-		m_vehicle = GetComponent<VehicleController>();
-		}
+    void OnEnable() {
+      m_vehicle = GetComponent<VehicleController>();
+    }
 
 
-	void Update ()
-		{
-		// Steering wheel rotation
+    void Update() {
+      // Steering wheel rotation
 
-		if (steeringWheel != null)
-			{
-			Vector3 angles = steeringWheel.localEulerAngles;
+      if (steeringWheel != null) {
+        Vector3 angles = steeringWheel.localEulerAngles;
+        float zRotation;
+        if (m_vehicle.maxSteerAngle > 0.0f) {
+          zRotation = -0.5f * degreesOfRotation * m_vehicle.steerAngle / m_vehicle.maxSteerAngle;
+        } else {
+          zRotation = 0.0f;
+        }
 
-			if (m_vehicle.maxSteerAngle > 0.0f)
-				angles.z = -0.5f * degreesOfRotation * m_vehicle.steerAngle / m_vehicle.maxSteerAngle;
-			else
-				angles.z = 0.0f;
+        angles.z = Mathf.Lerp(angles.z, zRotation, Time.deltaTime * 10f);
 
-			steeringWheel.localEulerAngles = angles;
-			}
+        steeringWheel.localEulerAngles = angles;
+      }
 
-		// Brake lights
+      // Brake lights
 
-		bool brakes = m_vehicle.brakeInput > 0.1f;
-		if (brakes != m_prevBrakes)
-			{
-			if (brakesRenderer != null && brakesMaterialIndex >= 0 && brakesMaterialIndex < brakesRenderer.sharedMaterials.Length)
-				{
-				Material[] materialsCopy = brakesRenderer.materials;
-				Destroy(materialsCopy[brakesMaterialIndex]);
-				materialsCopy[brakesMaterialIndex] = brakes? brakesOnMaterial : brakesOffMaterial;
-				brakesRenderer.materials = materialsCopy;
-				}
+      bool brakes = m_vehicle.brakeInput > 0.1f;
+      if (brakes != m_prevBrakes) {
+        if (brakesRenderer != null && brakesMaterialIndex >= 0 && brakesMaterialIndex < brakesRenderer.sharedMaterials.Length) {
+          Material[] materialsCopy = brakesRenderer.materials;
+          Destroy(materialsCopy[brakesMaterialIndex]);
+          materialsCopy[brakesMaterialIndex] = brakes ? brakesOnMaterial : brakesOffMaterial;
+          brakesRenderer.materials = materialsCopy;
+        }
 
-			m_prevBrakes = brakes;
-			}
-		}
-	}
+        m_prevBrakes = brakes;
+      }
+    }
+  }
 }
