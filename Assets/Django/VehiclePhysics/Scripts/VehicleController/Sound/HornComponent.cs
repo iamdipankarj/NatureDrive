cd6b9e9c5b8d64bb8b605bba23bbc54519986e7b
Associate Tech Lead (Frontend) <iamdipankarj@gmail.com>
2023-09-18 15:47:23 +0530

    Add Remaining input actions to solace for all sorts of vehicle controls

--- Assets/Django/VehiclePhysics/Scripts/VehicleController/Sound/HornComponent.cs
+++ Assets/Django/VehiclePhysics/Scripts/VehicleController/Sound/HornComponent.cs
@@ -7,86 +7,69 @@ using UnityEditor;
 #endif
 
 
-namespace Django.VehiclePhysics.Sound.SoundComponents
-{
-    /// <summary>
-    ///     Vehicle horn sound.
-    /// </summary>
-    [Serializable]
-    public partial class HornComponent : SoundComponent
-    {
-        public override GameObject ContainerGO
-        {
-            get { return vehicleController.soundManager.otherSourceGO; }
-        }
-
-        public override AudioMixerGroup AudioMixerGroup
-        {
-            get { return vehicleController.soundManager.otherMixerGroup; }
-        }
-
-        public override bool InitLoop
-        {
-            get { return true; }
-        }
-
-        public override int Priority
-        {
-            get { return 200; }
-        }
-
-
-        public override void VC_Update()
-        {
-            base.VC_Update();
-            if (vehicleController.input.Horn)
-            {
-                SetVolume(baseVolume);
-                if (!source.isPlaying)
-                {
-                    Play();
-                }
-            }
-            else if (source.isPlaying)
-            {
-                Stop();
-            }
-        }
-
-
-        public override void VC_SetDefaults()
-        {
-            base.VC_SetDefaults();
-
-            if (Clip == null)
-            {
-                AddDefaultClip("Horn");
-            }
-        }
+namespace Django.VehiclePhysics.Sound.SoundComponents {
+  /// <summary>
+  ///     Vehicle horn sound.
+  /// </summary>
+  [Serializable]
+  public partial class HornComponent : SoundComponent {
+    public override GameObject ContainerGO {
+      get { return vehicleController.soundManager.otherSourceGO; }
     }
+
+    public override AudioMixerGroup AudioMixerGroup {
+      get { return vehicleController.soundManager.otherMixerGroup; }
+    }
+
+    public override bool InitLoop {
+      get { return true; }
+    }
+
+    public override int Priority {
+      get { return 200; }
+    }
+
+
+    public override void VC_Update() {
+      base.VC_Update();
+      if (vehicleController.input.Horn) {
+        SetVolume(baseVolume);
+        if (!source.isPlaying) {
+          Play();
+        }
+      } else if (source.isPlaying) {
+        Stop();
+      }
+    }
+
+
+    public override void VC_SetDefaults() {
+      base.VC_SetDefaults();
+
+      if (Clip == null) {
+        AddDefaultClip("Horn");
+      }
+    }
+  }
 }
 
 
 
 #if UNITY_EDITOR
 
-namespace Django.VehiclePhysics.Sound.SoundComponents
-{
-    [CustomPropertyDrawer(typeof(HornComponent))]
-    public partial class HornComponentDrawer : SoundComponentDrawer
-    {
-        public override bool OnNUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            if (!base.OnNUI(position, property, label))
-            {
-                return false;
-            }
+namespace Django.VehiclePhysics.Sound.SoundComponents {
+  [CustomPropertyDrawer(typeof(HornComponent))]
+  public partial class HornComponentDrawer : SoundComponentDrawer {
+    public override bool OnNUI(Rect position, SerializedProperty property, GUIContent label) {
+      if (!base.OnNUI(position, property, label)) {
+        return false;
+      }
 
 
-            drawer.EndProperty();
-            return true;
-        }
+      drawer.EndProperty();
+      return true;
     }
+  }
 }
 
 #endif
