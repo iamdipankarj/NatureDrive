cefd691f3a3c04a292239e74ad0757a91d7870b0
Associate Tech Lead (Frontend) <iamdipankarj@gmail.com>
2023-09-25 21:31:38 +0530

    Remove Django

--- Assets/Django/VehiclePhysics/Scripts/VehicleController/Effects/Effect.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Django.VehiclePhysics.Effects
-{
-    /// <summary>
-    ///     Base class for all vehicle effects.
-    /// </summary>
-    public abstract class Effect : VehicleComponent
-    {
-    }
-}
