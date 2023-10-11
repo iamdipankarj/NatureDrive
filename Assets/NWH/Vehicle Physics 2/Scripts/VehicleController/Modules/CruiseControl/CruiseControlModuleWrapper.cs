bf77e35b2c81685c2b54408a86c07652f7e3dde2
Associate Tech Lead (Frontend) <iamdipankarj@gmail.com>
2023-10-11 18:38:41 +0530

    Add Cruise module

--- Assets/NWH/Vehicle Physics 2/Scripts/VehicleController/Modules/CruiseControl/CruiseControlModuleWrapper.cs
+++ Assets/NWH/Vehicle Physics 2/Scripts/VehicleController/Modules/CruiseControl/CruiseControlModuleWrapper.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 
-namespace NWH.VehiclePhysics2.Modules.CruiseControl
-{
-    /// <summary>
-    ///     MonoBehaviour wrapper for CruiseControl module.
-    /// </summary>
-    [Serializable]
-    public partial class CruiseControlModuleWrapper : ModuleWrapper
-    {
-        public CruiseControlModule module = new CruiseControlModule();
+namespace NWH.VehiclePhysics2.Modules.CruiseControl {
+  /// <summary>
+  ///     MonoBehaviour wrapper for CruiseControl module.
+  /// </summary>
+  [Serializable]
+  public partial class CruiseControlModuleWrapper : ModuleWrapper {
+    public CruiseControlModule module = new();
 
-
-        public override VehicleComponent GetModule()
-        {
-            return module;
-        }
-
-
-        public override void SetModule(VehicleComponent module)
-        {
-            this.module = module as CruiseControlModule;
-        }
+    public override VehicleComponent GetModule() {
+      return module;
     }
+
+    public override void SetModule(VehicleComponent module) {
+      this.module = module as CruiseControlModule;
+    }
+  }
 }
